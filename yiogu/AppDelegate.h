43a804dedb85b3270c5e0d0173e5164918d83934
hdc <hdc@xuedingedeliumaojia.local>
2015-08-01 10:42:14 +0800

    Initial Commit

---
+++ yiogu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yiogu
+//
+//  Created by hdc on 15/8/1.
+//  Copyright (c) 2015年 hdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yiogu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yiogu
+//
+//  Created by hdc on 15/8/1.
+//  Copyright (c) 2015年 hdc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
